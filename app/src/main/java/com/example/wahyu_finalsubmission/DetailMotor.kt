90f081ba5ea106f550c31251fcf91dd789039d8e
whymaull <first.wahyumaul21@gmail.com>
2023-08-31 18:05:45 +0700

    add Action dialPhone

--- app/src/main/java/com/example/wahyu_finalsubmission/DetailMotor.kt
+++ app/src/main/java/com/example/wahyu_finalsubmission/DetailMotor.kt
@@ -1,19 +1,20 @@
 package com.example.wahyu_finalsubmission
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class DetailMotor : AppCompatActivity() {
+class DetailMotor : AppCompatActivity(), View.OnClickListener {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_detail_motor)
 
 		val selectedMotor = intent.getParcelableExtra<Motor>("motor")
-
 		selectedMotor?.let { motor ->
 			supportActionBar?.title = motor.nama
 			val imgDetailPhoto: ImageView = findViewById(R.id.img_detail_photo)
@@ -36,6 +37,19 @@ class DetailMotor : AppCompatActivity() {
 			tvArticleCC.text = motor.cc
 			tvArticleBahanBakar.text = motor.bahanBakar
 
+		}
+
+		val btnDialPhone: Button = findViewById(R.id.dialnumber)
+		btnDialPhone.setOnClickListener(this)
+	}
+
+	override fun onClick(v: View?) {
+		when (v?.id) {
+			R.id.dialnumber -> {
+				val phoneNumber = "0895384154732"
+				val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+				startActivity(dialPhoneIntent)
+			}
 		}
 	}
 }
